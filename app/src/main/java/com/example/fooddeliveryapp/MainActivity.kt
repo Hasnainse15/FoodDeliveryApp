ff321b13f4c140faf9dc266a462a05bd863ac154
pryze-Hasnain <husnain@pryze.pk>
2022-01-04 11:09:01 +0500

    first commint

---
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fooddeliveryapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //hababhabbdsbe
+        //jkknknd
+
+        //first commit
+
+
+    }
+}
