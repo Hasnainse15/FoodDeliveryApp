e77826f64a7397c909c57257e3a3d02437375080
pryze-Hasnain <husnain@pryze.pk>
2022-01-04 11:55:34 +0500

    main activity change

--- app/src/main/java/com/example/fooddeliveryapp/ui/main/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/fooddeliveryapp/ui/main/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fooddeliveryapp
+package com.example.fooddeliveryapp.ui.main.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fooddeliveryapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
